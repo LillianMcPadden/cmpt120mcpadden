2e9e54a874c4e9625c82f58c0720d85157877e1e
LillianMcPadden <Lillian.McPadden1@marist.edu>
2021-09-29 16:48:02 -0400

    10/1/21 question 3

---
+++ stringToNumbers.py
@@ -0,0 +1,6 @@
+# stringToNumbers.py
+# This function is a list of numbers written as strings, 
+# Each entry is modified in the list by converting the string to a number
+
+def toNumbers(strList):
+    return [int(i) for i in strList]
