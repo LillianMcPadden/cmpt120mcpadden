3659af30eb696211e3830334612b1b20b4f6d98d
LillianMcPadden <Lillian.McPadden1@marist.edu>
2021-10-21 11:58:45 -0400

    10/22/21 Question 4

---
+++ syracuse.py
@@ -0,0 +1,18 @@
+# syracuse.py
+# This program gets a starting value from the user and then prints the
+# syracuse sequence for that starting value
+
+def syracuse(x, list = None):
+    if list is None:
+        list = []
+    if x == 1:
+        list.append(x)
+        return list
+    elif x % 2== 0:
+        x = x/ 2
+        list.append(x)
+        return syracuse(x, list)
+    else:
+        x = (3 * x) + 1
+        list.append(x)
+        return syracuse(x, list)
