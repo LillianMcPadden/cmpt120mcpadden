23b6cb524f2c8a3c7eb363b9021ffeb6155f949c
LillianMcPadden <Lillian.McPadden1@marist.edu>
2021-11-02 20:54:43 -0400

    Mod 8 Question 2

--- craps_sim.py
+++ craps_sim.py
@@ -7,17 +7,23 @@ def roll():
     return dice
 
 def main():
-    d1 = roll()
-    d2 = roll()
-    result = d1 + d2
-    
-    # check if its a win
-    if result == 7 or result == 11:
-        print("Winner!", d1,d2)
-    elif result == 2 or result == 3 or result == 12:
-        print("You lose, sorry!",d1,d2)
-    else:
-        print("Points: ",result, d1,d2)
+    count = 0
+    rolls = 0
+    while (count == 0):
+        d1 = roll()
+        d2 = roll()
+        result = d1 + d2
+        if result == 2 or result == 3 or result == 12:
+            count = count - 1
+            print("You lose, sorry!",d1,d2)
+        elif result == 7 or result == 11:
+            count = count + 1
+            print("Winner!", d1,d2)
+        else:
+            print("Points: ",d1,d2)
+        rolls = rolls + 1
+
+    print('The probability of winning is', (count / rolls))
 main()
 
 
