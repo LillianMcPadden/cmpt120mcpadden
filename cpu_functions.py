1e66b2e739d1919ffea1b800a20a9c67462fb07f
Juan Arias <juan.arias@marist.edu>
2021-11-08 21:59:27 -0500

    Feedback

--- cpu_functions.py
+++ cpu_functions.py
@@ -1,26 +1,33 @@
 # cpu_functions.py
+from logic_gates import * 
 
 def one_bit_adder(a,b,ci):
-    if a + b + ci == 0:
-        sumb = 0
-        co = 0
-        print('sum: ', sumb,
-              'co: ', co)
-    elif a + b + ci == 1:
-        sumb = 1
-        co = 0
-        print('sum: ', sumb,
-              'co: ', co)
-    elif a + b + ci == 2:
-        sumb = 0
-        co = 1
-        print('sum: ', sumb,
-              'co: ', co)
-    else:
-        sumb = 1
-        co = 1
-        print('sum: ', sumb,
-              'co: ', co)
+    # if a + b + ci == 0:
+    #     sumb = 0
+    #     co = 0
+    #     print('sum: ', sumb,
+    #           'co: ', co)
+    # elif a + b + ci == 1:
+    #     sumb = 1
+    #     co = 0
+    #     print('sum: ', sumb,
+    #           'co: ', co)
+    # elif a + b + ci == 2:
+    #     sumb = 0
+    #     co = 1
+    #     print('sum: ', sumb,
+    #           'co: ', co)
+    # else:
+    #     sumb = 1
+    #     co = 1
+    #     print('sum: ', sumb,
+    #           'co: ', co)
+    xor_t = xor_g(a,b)
+    sumb = xor_g(xor_t,ci)
+    and_1 = and_g(xor_t,ci)
+    and_2 = and_g(a,b)
+    co = or_g(and_1,and_2)
+    return sumb, co
 
 def main():
     a = int(input('Enter a number (a) either 0 or 1: '))
--- logic_gates.py
+++ logic_gates.py
@@ -19,16 +19,20 @@ def not_g(a):
         return 1
 
 def nand_g(a,b):
-    if a == 0 or b == 0:
-        return 1
-    else:
-        return 0
+    # if a == 0 or b == 0:
+    #     return 1
+    # else:
+    #     return 0
+    return not_g(and_g(a,b))
 
 def xor_g(a,b):
-    if (a == 0 and b == 0) or (a == 1 and b == 1):
-        return 0
-    else:
-        return 1
+    # if (a == 0 and b == 0) or (a == 1 and b == 1):
+    #     return 0
+    # else:
+    #     return 1
+    and_1 = and_g(not_g(a),b)
+    and_2 = and_g(a,not_g(b))
+    return or_g(and_1,and_2)
 
 def main():
     a = int(input("enter a number (a) either 0 or 1: "))
